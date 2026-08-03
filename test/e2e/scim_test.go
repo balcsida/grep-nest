a4eab2aacf56043ff314b56a01946688f590b907
Dávid Balatoni <balcsida@gmail.com>
2026-08-03 16:36:52 +0200

    fix(test): update SCIM OIDC wiring

--- test/e2e/scim_test.go
+++ test/e2e/scim_test.go
@@ -16,6 +16,7 @@ import (
 
 	"github.com/grepnest/grepnest/internal/account"
 	"github.com/grepnest/grepnest/internal/admin"
+	"github.com/grepnest/grepnest/internal/audit"
 	"github.com/grepnest/grepnest/internal/authn"
 	"github.com/grepnest/grepnest/internal/authz"
 	"github.com/grepnest/grepnest/internal/config"
@@ -114,7 +115,7 @@ func newSCIME2EHandler(t *testing.T, database milestoneDatabase, idp *oidcTestPr
 	repositories := &repository.Service{Store: database.store}
 	searchService := search.NewService(oidcSearchBackend{}, authorizer, search.Limits{MaxResults: 10, MaxResponseBytes: 64 << 10})
 	mux := http.NewServeMux()
-	httpapi.RegisterAuth(mux, false, false, true, []sso.Provider{&oidcclient.Provider{Client: client, Store: database.store, Sessions: sessions, LoginTTL: time.Minute}}, requestAuth, sessions, nil)
+	httpapi.RegisterAuth(mux, false, false, true, []sso.Provider{oidcclient.NewProvider(client, database.store, sessions, nil, time.Minute)}, requestAuth, sessions, nil)
 	httpapi.RegisterRepositories(mux, requestAuth, repositories, 64<<10, 10, 64<<10)
 	httpapi.RegisterSearch(mux, requestAuth, searchService, 64<<10, 64<<10)
 	httpapi.RegisterAccount(mux, requestAuth, &account.Service{Manager: tokens, Authorizer: authorizer}, 64<<10, 64<<10)
